2b5a1e2737c07788c8bef8bd8accd5f0ed01cfb0
003miles <72564541+003miles@users.noreply.github.com>
2021-03-07 13:42:14 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ config = {
     'password': 'password123',  # login goes here
     'IGNs': ['notch', 'steve'],     # Enter IGNs of players to get logs for
     'channel': 'https://discord.com/channels/yourDiscordServersMCChatLink',      # Replace with your own link
-    'searchSuffix': 'the server'    # Will search chat in format {ign} {searchSuffix} e.g. 'notch the server' will return all instances of 'notch left the server' and 'notch joined the server'. (Optional)
+    'searchSuffix': 'the server'    # (Optional) Will search chat in format {ign} {searchSuffix} e.g. 'notch the server' will return all instances of 'notch left the server' and 'notch joined the server'. 
 }
